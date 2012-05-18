c47553fcacd684d2a39f347c112b0b7a43fedefb
kine_cp <SND\kine_cp>
2012-05-18 11:02:23 +0000

    Init version, copy of original NVR_DynamicsNavProtoclHandler v1.0.2.0

---
+++ Development/DynamicsNAVProtocolHandler/NVR_DynamicsNAVProtocolHandler/App.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Win32Helper;
+using System.Diagnostics;
+using System.IO;
+
+namespace NVR_DynamicsNAVProtocolHandler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static bool haveParams = false;
+        private void Application_Startup_1(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                var args = "";
+                foreach (String a in e.Args)
+                {
+                    if (args != "")
+                        args += " ";
+                    args += a;
+                }
+                if (args == "")
+                    return;
+
+                haveParams = true;
+                uint pid = 0;
+                try
+                {
+                    Win32Helper.Win32.User32.GetWindowThreadProcessId(Win32.User32.GetForegroundWindow(), out pid);
+                    Process proc = System.Diagnostics.Process.GetProcessById((int)pid);
+                    String activeProcess = proc.MainModule.FileName;
+                    String path = Path.GetDirectoryName(activeProcess) + @"\";
+
+                    if (Path.GetFileName(activeProcess).ToLower() == "finsql.exe")
+                    {
+                        if (File.Exists(path + "Microsoft.Dynamics.Nav.Client.exe"))
+                        //Runs the client from same folder as calling process
+                        {
+                            RunProcess(path + "Microsoft.Dynamics.Nav.Client.exe", @"""" + args + @"""");
+                            return;
+                        }
+
+                        var versionInfo = FileVersionInfo.GetVersionInfo(activeProcess);
+                        var fileVersion = versionInfo.FileVersion;
+                        var navPath = FindNavClient(fileVersion, activeProcess);
+                        if (String.IsNullOrEmpty(navPath))
+                        {
+                            MessageBox.Show("Error", "Same version of RTC was not found!", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+                        RunProcess(navPath, @"""" + args + @"""");
+                        return;
+                    }
+                    else
+                    {
+                        string defaultPath = (String)Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default", "");
+                        RunProcess(defaultPath.Replace(@" ""%1""", ""), @"""" + args + @"""");
+                        return;
+                        //Use Default
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string defaultPath = (String)Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default", "");
+                    RunProcess(defaultPath.Replace(@" ""%1""", ""), @"""" + args + @"""");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static private void TraverseTree(string root, string filter, List<string> _navClientEXEList, string fileVersion)
+        {
+            // Data structure to hold names of subfolders to be
+            // examined for files.
+            Stack<string> dirs = new Stack<string>(20);
+
+            if (!System.IO.Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs;
+                try
+                {
+                    subDirs = System.IO.Directory.GetDirectories(currentDir);
+                }
+                // An UnauthorizedAccessException exception will be thrown if we do not have
+                // discovery permission on a folder or file. It may or may not be acceptable 
+                // to ignore the exception and continue enumerating the remaining files and 
+                // folders. It is also possible (but unlikely) that a DirectoryNotFound exception 
+                // will be raised. This will happen if currentDir has been deleted by
+                // another application or thread after our call to Directory.Exists. The 
+                // choice of which exceptions to catch depends entirely on the specific task 
+                // you are intending to perform and also on how much you know with certainty 
+                // about the systems on which this code will run.
+                catch (UnauthorizedAccessException e)
+                {
+                    //Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    //Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                string[] files = null;
+                try
+                {
+                    files = System.IO.Directory.GetFiles(currentDir, filter);
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+
+                    //Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    //Console.WriteLine(e.Message);
+                    continue;
+                }
+                // Perform the required action on each file here.
+                // Modify this block to perform your required task.
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        string version = System.Diagnostics.FileVersionInfo.GetVersionInfo(file).FileVersion;
+                        if (fileVersion == version)
+                        {
+                            _navClientEXEList.Add(file);
+                        }
+                    }
+                    catch (System.IO.FileNotFoundException e)
+                    {
+                        // If file was deleted by a separate application
+                        //  or thread since the call to TraverseTree()
+                        // then just continue.
+                        //Console.WriteLine(e.Message);
+                        continue;
+                    }
+                }
+
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+        }
+
+
+        private string FindNavClient(string fileVersion,string currentFolder)
+        {
+            string fileName = "";
+            var progress = new Progress();
+            progress.Show();
+            progress.Topmost = true;
+            try
+            {
+                var root = Directory.GetParent(currentFolder).Parent;
+                List<String> files = new List<string>();
+                TraverseTree(root.FullName, "Microsoft.Dynamics.Nav.Client.exe", files, fileVersion);
+            }
+            catch (Exception e)
+            {
+            }
+            finally
+            {
+                progress.Close();
+            }
+            return fileName;
+        }
+
+        private void RunProcess(string path, string param)
+        {
+            var baloon = new Toaster(path);
+            if (NVR_DynamicsNAVProtocolHandler.Properties.Settings.Default.ShowFileInfo)
+            {
+                baloon.Show();
+            }
+            var procInfo = new ProcessStartInfo(path, param);
+            procInfo.WorkingDirectory = Path.GetDirectoryName(path);
+            //procInfo.FileName = Path.GetFileName(path);
+
+            Process.Start(procInfo);
+            if (NVR_DynamicsNAVProtocolHandler.Properties.Settings.Default.ShowMessageBox)
+            {
+               MessageBox.Show("Starting this command:\n"+procInfo.FileName+@""""+param+@"""");
+            }
+            if (!NVR_DynamicsNAVProtocolHandler.Properties.Settings.Default.ShowFileInfo)
+                Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Development/DynamicsNAVProtocolHandler/NVR_DynamicsNAVProtocolHandler/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace NVR_DynamicsNAVProtocolHandler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (NVR_DynamicsNAVProtocolHandler.App.haveParams)
+            {
+                this.Hide();
+            }
+            CheckIfActive();
+            versionLabel.Content = "ver "+System.Diagnostics.FileVersionInfo.GetVersionInfo(Process.GetCurrentProcess().MainModule.FileName).FileVersion;
+        }
+
+        private void CheckIfActive()
+        {
+            string defaultPath = (String)Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default", "XXX");
+            var isActive = (defaultPath != "XXX") && (defaultPath != "");
+            button.Content = (isActive ? "Deactivate" : "Activate");
+        }
+
+        private void ChangeActive()
+        {
+            string defaultPath = (String)Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default", "XXX");
+            var isActive = (defaultPath != "XXX") && (defaultPath != "");
+            if (isActive)
+            {
+                DeactivateHandler();
+            }
+            else
+            {
+                ActivateHandler();
+            }
+            CheckIfActive();
+        }
+
+        private void ActivateHandler()
+        {
+            string defaultPath = (String)Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "", "");
+            Microsoft.Win32.Registry.SetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default",defaultPath,Microsoft.Win32.RegistryValueKind.String);
+            Microsoft.Win32.Registry.SetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "", Process.GetCurrentProcess().MainModule.FileName + @" ""%1""");
+        }
+
+        private void DeactivateHandler()
+        {
+            string defaultPath = (String)Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default", "");
+            Microsoft.Win32.Registry.SetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "", defaultPath);
+            Microsoft.Win32.Registry.SetValue(@"HKEY_CLASSES_ROOT\DYNAMICSNAV\Shell\Open\Command", "Default", "");
+        }
+
+        private void buttonClose_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ChangeActive();
+        }
+    }
+}
