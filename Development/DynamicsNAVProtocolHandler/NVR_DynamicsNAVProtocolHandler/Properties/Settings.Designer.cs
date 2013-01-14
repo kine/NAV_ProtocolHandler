d2bc7e46e2675dcfe4d8b5e46bfbe3c0e4a9c126
kine_cp <SND\kine_cp>
2013-01-14 14:01:35 +0000

    Automatic mapping (SQL Server = NAV Server and DB=Instance) could be enabled by AutoMapping setting.

--- Development/DynamicsNAVProtocolHandler/NVR_DynamicsNAVProtocolHandler/Properties/Settings.Designer.cs
+++ Development/DynamicsNAVProtocolHandler/NVR_DynamicsNAVProtocolHandler/Properties/Settings.Designer.cs
@@ -70,5 +70,17 @@ namespace NVR_DynamicsNAVProtocolHandler.Properties {
                 this["DisableMapping"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool AutoMapping {
+            get {
+                return ((bool)(this["AutoMapping"]));
+            }
+            set {
+                this["AutoMapping"] = value;
+            }
+        }
     }
 }
