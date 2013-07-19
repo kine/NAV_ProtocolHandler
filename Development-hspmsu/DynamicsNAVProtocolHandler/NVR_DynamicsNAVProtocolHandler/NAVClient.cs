6b9e1e789a5f3fbff5971a9511283e68e190984d
hspmsu_cp <SND\hspmsu_cp>
2013-07-19 09:41:42 +0000

    Use PreRun to create settingsfile and added uri handling from non-nav applications based on "?buildversion=NAVBuild" extentsion

--- Development-hspmsu/DynamicsNAVProtocolHandler/NVR_DynamicsNAVProtocolHandler/NAVClient.cs
+++ Development-hspmsu/DynamicsNAVProtocolHandler/NVR_DynamicsNAVProtocolHandler/NAVClient.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace NVR_DynamicsNAVProtocolHandler
 {
@@ -105,7 +108,88 @@ namespace NVR_DynamicsNAVProtocolHandler
             Uri = _uri;
         }
 
-        abstract public void RunClient(); 
+        public void createSettingsFile()
+        {
+            String stdCUS = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\Microsoft\\Microsoft Dynamics NAV";
+            String userCUS = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Microsoft Dynamics NAV";
+
+            Hashtable props = new Hashtable();
+            if (Uri.Split('/')[2].Contains(":"))
+            {
+                props.Add("Server", Uri.Split('/')[2].Split(':')[0]);
+                props.Add("ServerPort", Uri.Split('/')[2].Split(':')[1]);
+            }
+            else
+            {
+                props.Add("Server", Uri.Split('/')[2]);
+                props.Add("ServerPort", NVR_DynamicsNAVProtocolHandler.Properties.Settings.Default.NAVServerDefaultPort);
+            }
+            props.Add("ServerInstance", Uri.Split('/')[3]);
+            props.Add("UrlHistory", "");
+            props.Add("UnknownSpnHint", "");
+
+            FileInfo CUS = new FileInfo(stdCUS + "\\ClientUserSettings.config");
+
+            FileInfo preparedCUS = new FileInfo(userCUS + "\\" + props["Server"] + "_" + props["ServerInstance"] + ".config");
+
+            if (preparedCUS.Exists)
+            {
+                Param = " -settings:\"" + preparedCUS.FullName + "\" " + Param;
+                return;
+            }
+
+            if (CUS != null)
+            {
+                if (CUS.Exists)
+                {
+                    try
+                    {
+                        //Create the XmlDocument.
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(CUS.FullName);
+                        XmlNode node = doc.SelectSingleNode("//appSettings");
+
+                        foreach (string parameter in new string[] { "UnknownSpnHint", "UrlHistory", "ServerPort", "ServerInstance", "Server" })
+                        {
+                            XmlElement elem = (XmlElement)node.SelectSingleNode(string.Format("//add[@key='{0}']", parameter));
+
+                            if (elem != null)
+                            {
+                                // set value for key
+                                elem.SetAttribute("value", (string)props[parameter]);
+                            }
+                            else
+                            {
+                                string tempstr = (string)props[parameter];
+                                // key was not found so create the 'add' element
+                                // and set it's key/value attributes
+                                elem = doc.CreateElement("add");
+                                elem.SetAttribute("key", parameter);
+                                elem.SetAttribute("value", tempstr);
+                                node.AppendChild(elem);
+                            }
+                        }
+                        doc.Save(preparedCUS.FullName);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+
+            if (preparedCUS.Exists)
+                Param = " -settings:\"" + preparedCUS.FullName + "\" " + Param;
+            else
+            {
+                FileInfo dfltUserCUS = new FileInfo(userCUS + "\\ClientUserSettings.config");
+                if (dfltUserCUS.Exists)
+                    Param = " -settings:\"" + dfltUserCUS.FullName + "\" " + Param;
+            }
+        }
+
+        abstract public void PreRun();
+        abstract public void PostRun();
+        abstract public void RunClient();
     }
 
     /// <summary>
@@ -120,9 +204,21 @@ namespace NVR_DynamicsNAVProtocolHandler
         /// </summary>
         public override void RunClient()
         {
-            var procInfo = new ProcessStartInfo(Path, @"""" + Uri + @"""");
+            var procInfo = new ProcessStartInfo(Path, Param + @"""" + Uri + @"""");
             procInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(Path);
+            PreRun();
             Process.Start(procInfo);
+            PostRun();
+        }
+
+        public override void PreRun()
+        {
+            if (NVR_DynamicsNAVProtocolHandler.Properties.Settings.Default.CreateSettingsFile)
+                createSettingsFile();
+        }
+
+        public override void PostRun()
+        {
         }
     }
 }
